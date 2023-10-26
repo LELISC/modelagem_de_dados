834dd36e776b65bddf0eaa1b7152d2db3471dd37
Filipe Lelis <105754368+LELISC@users.noreply.github.com>
2023-10-26 20:37:36 -0300

    Add files via upload

---
+++ hospital_web_site (3).sql
@@ -0,0 +1,16 @@
+ALTER TABLE `consultas` DROP FOREIGN KEY `consultas_fk0`;
+
+ALTER TABLE `paciente` DROP FOREIGN KEY `paciente_fk0`;
+
+ALTER TABLE `paciente` DROP FOREIGN KEY `paciente_fk1`;
+
+ALTER TABLE `especialista` DROP FOREIGN KEY `especialista_fk0`;
+
+ALTER TABLE `especialista` DROP FOREIGN KEY `especialista_fk1`;
+
+ALTER TABLE `quarto` DROP FOREIGN KEY `quarto_fk0`;
+
+ALTER TABLE `internacao` DROP FOREIGN KEY `internacao_fk0`;
+
+ALTER TABLE `quarto_1` DROP FOREIGN KEY `quarto_1_fk0`;
+
